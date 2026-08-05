b93a0bb585c58159d9386c27c51f323dbce271d2
Zaloguj12 <zaloguj12@proton.me>
2026-08-05 14:36:51 +0200

    add fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
 vid_1 = "videos/1.mp4"
 vid_2 = "videos/2.mp4"
 vid_3 = "videos/3.mp4"
 
 videos = [vid_1, vid_2, vid_3]
+
+@app.get("/videos/")
+def watch():
+    rand_num = random.randint(0, 2)
+    print(videos[rand_num])
+    return
