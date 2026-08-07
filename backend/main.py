53ff84d9f8ab0d74a3dba75260e6586a406ff67b
Zaloguj12 <zaloguj12@proton.me>
2026-08-07 14:21:42 +0200

    prepare for new implementation

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI
 import random
+import json
 
 app = FastAPI()
 
-vid_1 = "videos/1.mp4"
-vid_2 = "videos/2.mp4"
-vid_3 = "videos/3.mp4"
-
-videos = [vid_1, vid_2, vid_3]
-
-@app.get("/videos/")
+@app.get("/video")
 def watch():
-    rand_num = random.randint(0, 2)
-    print(videos[rand_num])
     return
