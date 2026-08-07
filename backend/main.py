df1f8336996f01c022e7a0823fb131c6afcda976
Zaloguj12 <zaloguj12@proton.me>
2026-08-07 14:36:30 +0200

    implement random video selection in python

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import random
 import json
 
 app = FastAPI()
 
+app.mount("/videos", StaticFiles(directory="videos"), name="videos")
+
+with open("videos.json", "r", encoding="utf-8") as f:
+    db = json.load(f)
+
 @app.get("/video")
 def watch():
-    return
+    return random.choice(db["videos"])
